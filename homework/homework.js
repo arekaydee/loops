e332174110b99658b40989ed625494bca44cfade
Raphael Koehler-Derrick <koehr998@newschool.edu>
2016-02-09 09:52:27 -0500

    Moving in a circular direction for this project, and making sure you do too, no matter what size screen you view it on!

--- homework/homework.js
+++ homework/homework.js
@@ -4,6 +4,15 @@
 
 function setup(){
   createCanvas(windowWidth,windowHeight);
-  
-  ellipse(windowWidth/2,windowHeight/2,100,100);
+
+for(var i = 0; i <50; i++){
+  noStroke();
+  var c = color('rgba(0,50,255,.5)');
+  fill(c);
+  ellipse(random(windowWidth),random(windowHeight),75+i,75+i);
+  var c = color('rgba(255,0,158,.5)');
+  fill(c);
+  ellipse(random(windowWidth),random(windowHeight),75+i,75+i);
+
+  }
 }
